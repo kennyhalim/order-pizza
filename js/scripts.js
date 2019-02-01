87ab6ce86527a72fc2f402772185ba7b3b0330ae
Kenny Halim <kennyhalim98@gmail.com>
2019-02-01 10:09:04 -0800

    add calculateTotal prototype and create new pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,25 @@ Pizza.prototype.addToppings = function(topping){
   this.toppings.push(topping);
 }
 
+Pizza.prototype.calculateTotal = function(){
+  var total = 0;
 
+  if (this.size === "Small") {
+    total = total + 7;
+  } else if (this.size === "Medium") {
+    total = total + 9;
+  } else if (this.size === "Large") {
+    total = total + 11;
+  } else if (this.size === "Extra Large") {
+    total = total + 14;
+  }
+
+  this.toppings.forEach(function(topping){
+    total = total + 1;
+  })
+
+  return total;
+}
 
 
 //User Interface
@@ -18,7 +36,19 @@ $(document).ready(function(){
     var inputtedEmail = $("input#email").val();
     var inputtedPhone = $("input#phone").val();
 
+    var e = document.getElementById ("pizzaSize");
+    var sizeOfPizza = e.options [e.selectedIndex] .value;
+    sizeOfPizza = sizeOfPizza.split(' ');
+    var size = sizeOfPizza[0];
+    var pizza1 = new Pizza(size);
+    $("#order-receipt").html("<strong>" +size + " Pizza </strong></br> with these toppings:");
 
+
+    $("input:checkbox[name=pizzaToppings]:checked").each(function(){
+      var pizzaTopping = $(this).val();
+      pizza1.addToppings(pizzaTopping);
+      $("#order-toppings").append("<li>" + pizzaTopping + "</li>");
+    });
 
     $(".name").text(inputtedName);
     $(".email").text(inputtedEmail);
